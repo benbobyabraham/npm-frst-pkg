f66486789bc537584bd58290c3327ce1c40d62df
benbobyabraham <benbobyabraham@gmail.com>
2022-06-25 08:27:45 +0530

    first commit

---
+++ node_modules/npm-frst-pkg/README.md
@@ -0,0 +1,5 @@
+# This is my first package
+
+### Author: Ben Boby
+
+> This package was made for testing out how packages were created and deployed to the npm package registry. It will be deleted at a later time.
---
+++ test/test.js
@@ -0,0 +1,6 @@
+// For the require statement, use the name you gave your published package
+const removeArrayDuplicates = require('npm-frst-pkg') 
+
+// let myNums = [1,2,3,1,4,1,2,5,3,4]
+// let uniqueNums = removeArrayDuplicates(myNums)
+// console.log(uniqueNums)
